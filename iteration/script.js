4580915a2ec465d88c627c26c9effc249413a8e9
原田 悠平 <114724659+Harada-TECH@users.noreply.github.com>
2026-06-18 18:45:52 +0900

    Add files via upload

---
+++ iteration/script.js
@@ -0,0 +1,13 @@
+// for文
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
+
+// 演習
+let total = 0; // totalの初期値は0
+
+for (let i = 0; i < 10; i++) {
+  total += i + 1;
+}
+
+console.log(total); // => 55
